db79355edd0b7e08b4d56de7d1d16c81122fb2d2
Mathis Holler <m.holler@orange.fr>
2025-03-03 14:12:21 +0100

    Version 1

---
+++ Exercice1.js
@@ -0,0 +1,15 @@
+/* Nous voulons notifier l’utilisateur si les nombres sont positifs ou négatifs.
+
+Ecris une condition qui affichera la nature du chiffre (positif, négatif…) dans la console.
+
+Déclare une variable qui contiendra un nombre
+et teste ta condition avec les valeurs suivantes que tu donneras à la variable : 6, -8 et 0.*/ 
+
+let myNumber = 5
+
+if(myNumber > 0) {
+console.log("Mon nombre est positif")
+} else {
+    console.log ("Mon nombre est négatif")
+}
+
